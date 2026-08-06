a843e70ce799529ea80b099091174902056c895b
s-t-e-v <steven.bandaogo@proton.me>
2026-08-06 23:51:49 +0200

    fix: make p3 setup idempotent

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -2,11 +2,15 @@
 set -e
 
 # k3d
-k3d cluster create --config "confs/k3d-cluster.yaml"
+CLUSTER_NAME="k3d-cluster"
+
+if ! k3d cluster list "$CLUSTER_NAME" --no-headers >/dev/null 2>&1; then
+	k3d cluster create --config "confs/k3d-cluster.yaml"
+fi
 
 
 kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl wait --for=condition=available --timeout=600s deployment/argocd-applicationset-controller -n argocd
+kubectl wait --for=condition=Ready pod --all -n argocd --timeout=600s
 
 kubectl apply -n argocd -f confs/application.yaml
